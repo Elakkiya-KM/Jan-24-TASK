3daf5f9721855a27beb4f429bdbe18aa62f3ca0a
Elakkiya KM <elakkiyakmfuture@gmail.com>
2024-01-27 16:01:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// Start the countdown
+setTimeout(function () {
+  console.log(10);
+
+  // Continue the countdown
+  setTimeout(function () {
+    console.log(9);
+
+    // Continue the countdown
+    setTimeout(function () {
+      console.log(8);
+
+      // Continue the countdown
+      setTimeout(function () {
+        console.log(7);
+
+        // Continue the countdown
+        setTimeout(function () {
+          console.log(6);
+
+          // Continue the countdown
+          setTimeout(function () {
+            console.log(5);
+
+            // Continue the countdown
+            setTimeout(function () {
+              console.log(4);
+
+              // Continue the countdown
+              setTimeout(function () {
+                console.log(3);
+
+                // Continue the countdown
+                setTimeout(function () {
+                  console.log(2);
+
+                  // Continue the countdown
+                  setTimeout(function () {
+                    console.log(1);
+
+                    // Display "Happy Independence Day"
+                    setTimeout(function () {
+                      console.log("Happy Independence Day");
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
